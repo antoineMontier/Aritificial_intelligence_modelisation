907e4a472ced2a4f8a26ea82020f0c2e37209d9d
Antoine <antoine.montier@sfr.fr>
2022-12-04 01:01:49 +0100

    the seed is now an array of commands performed seg fault to fix

--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <time.h>
-#define FRAMES_PER_SECOND 1000
-#define PARTICLES_NUMBER 100
+#define FRAMES_PER_SECOND 60
+#define PARTICLES_NUMBER 200
 #define NB_COLORS 3
 #define ADN_TRANSMISSION 0.5
+#define COMMAND_NUMBER 1000 // 5 commands : 0 nothing 1 up 2 down 3 left 4 right
+#define PARTICLE_SPEED 5
 
 typedef struct
 {
+    int round;
     double p_x;
     double p_y;
     double x;
     double y;
     double size;
-    double seed_x;
-    double seed_y;
+    int* seed;
     SDL_Color color;
 } particle;
 
@@ -25,8 +27,9 @@ void draw_particle(SDL_Renderer *r, particle p);
 int move_particle(particle *p); // returns 0 if particle in the box and 1 if outside
 void display_particle_informations(SDL_Renderer *r, TTF_Font *f, particle p, char *tmp, const char *position);
 int initialise_particle(particle *p, SDL_Color *c);
-void inherit_particle(particle *source, particle *target, SDL_Color *c);
+void inherit_particle(particle *source, particle *target);
 void copy_particle(particle *source, particle *target);
+void close_Particle(particle **p);
 
 int main()
 { /*gcc -c -Wall -Wextra main.c && gcc main.o -lm -o main && ./main*/
@@ -65,6 +68,7 @@ int main()
     char *tmp = malloc(10);
     particle* part = malloc(PARTICLES_NUMBER*sizeof(particle));
     particle temp;
+    initialise_particle(&temp, colors);
     for(int i=0; i< PARTICLES_NUMBER; i++)
         initialise_particle(&part[i], colors);
 
@@ -72,13 +76,13 @@ int main()
 
     while (program_launched)
     {
-
+        printf("%d\n", iterations);
         for(int i = 0; i < PARTICLES_NUMBER ; i++){
             if(move_particle(&part[i]) == 0){
                 printf("regeneration\n");
                 copy_particle(&part[i], &temp);
                 for(int j = 0; j < PARTICLES_NUMBER ; j++)
-                    inherit_particle(&temp, &part[j], colors);
+                    inherit_particle(&temp, &part[j]);
                 iterations = 0;
                 i = PARTICLES_NUMBER;
             }
@@ -126,7 +130,7 @@ int main()
         SDL_RenderPresent(r); // refresh the render
         SDL_Delay(1000 / FRAMES_PER_SECOND);
     }
-    free(part);
+    close_Particle(&part);
     free(colors);
     free(tmp);
     TTF_CloseFont(param_font);
@@ -155,40 +159,49 @@ void draw_particle(SDL_Renderer *r, particle p)
 int move_particle(particle *p)
 {
 
-    int touched = 0;
+    if(p->round >= COMMAND_NUMBER){
+        p->round = 0;
+        return -1;
+    }
+
     //============================== bounce =============================
     if (p->y - p->size < HEIGHT * 0.1)
-    {
         p->y = p->size + HEIGHT * 0.1;
-        touched = 1;
-    }
     else if (p->y + p->size > HEIGHT * 0.9)
-    {
         p->y = HEIGHT * 0.9 - p->size;
-        touched = 1;
-    }
     if (p->x - p->size < WIDTH * 0.1)
-    {
         p->x = WIDTH * 0.1 + p->size;
-        touched = 1;
-    }
     else if ((p->y - p->size < HEIGHT * 0.45 || p->y + p->size > HEIGHT * 0.55) && p->x + p->size > WIDTH * 0.9)
-    {
         p->x = WIDTH * 0.9 - p->size;
-        touched = 1;
-    }
-    if (fabs(p->p_x - p->x) < 0.05 && fabs(p->p_y - p->y) < 0.05 && touched)
-        return 1;
     p->p_x = p->x;
     p->p_y = p->y;
     //==================================================================
-
-    p->x += /*(rand() / (float)RAND_MAX - 0.5)*/ + p->seed_x;
-    p->y += /*(rand() / (float)RAND_MAX - 0.5)*/ + p->seed_y;
-
+    switch (p->seed[p->round])
+    {
+    case 1:
+        p->y -= PARTICLE_SPEED;
+        //printf("up\n");
+        break;
+    case 2:
+        p->y += PARTICLE_SPEED;
+        //printf("down\n");
+        break;
+    case 3:
+        p->x -= PARTICLE_SPEED;
+        //printf("left\n");
+        break;
+    case 4:
+        p->x += PARTICLE_SPEED;
+        //printf("right\n");
+        break;
+    
+    default:
+        break;
+    }
+    p->round += 1;
     if (p->x + p->size > WIDTH * 0.1 - 5 && p->y + p->size > HEIGHT * 0.1 - 5 && p->x - p->size < WIDTH * 0.9 + 5 && p->y - p->size < HEIGHT * 0.9 + 5)
     {
-        return 2;
+        return 1;
     }
     return 0;
 }
@@ -206,10 +219,7 @@ void display_particle_informations(SDL_Renderer *r, TTF_Font *f, particle p, cha
         text(r, WIDTH * 0, HEIGHT * 0.02, tmp, f, p.color.r, p.color.g, p.color.b);
         gcvt(p.y, 3, tmp);
         text(r, WIDTH * 0.1, HEIGHT * 0.02, tmp, f, p.color.r, p.color.g, p.color.b);
-        gcvt(p.seed_x, 4, tmp);
-        text(r, WIDTH * 0.2, HEIGHT * 0.02, tmp, f, p.color.r, p.color.g, p.color.b);
-        gcvt(p.seed_y, 4, tmp);
-        text(r, WIDTH * 0.3, HEIGHT * 0.02, tmp, f, p.color.r, p.color.g, p.color.b);
+
     }
     else if (strcmp(position, "down") == 0)
     {
@@ -223,17 +233,15 @@ void display_particle_informations(SDL_Renderer *r, TTF_Font *f, particle p, cha
         text(r, WIDTH * 0, HEIGHT * 0.97, tmp, f, p.color.r, p.color.g, p.color.b);
         gcvt(p.y, 3, tmp);
         text(r, WIDTH * 0.1, HEIGHT * 0.97, tmp, f, p.color.r, p.color.g, p.color.b);
-        gcvt(p.seed_x, 4, tmp);
-        text(r, WIDTH * 0.2, HEIGHT * 0.97, tmp, f, p.color.r, p.color.g, p.color.b);
-        gcvt(p.seed_y, 4, tmp);
-        text(r, WIDTH * 0.3, HEIGHT * 0.97, tmp, f, p.color.r, p.color.g, p.color.b);
+
     }
 }
 
 int initialise_particle(particle *p, SDL_Color *c)
 {
-    p->seed_x = rand() / (float)RAND_MAX - 0.5;
-    p->seed_y = rand() / (float)RAND_MAX - 0.5;
+    p->seed = malloc(COMMAND_NUMBER*sizeof(int));
+    for (int i = 0; i < COMMAND_NUMBER; i++)
+        p->seed[i] = rand() % 5;
     p->x = WIDTH / 2;
     p->y = HEIGHT / 2;
     p->size = 20;
@@ -242,27 +250,27 @@ int initialise_particle(particle *p, SDL_Color *c)
     p->color.g = c[col].g;
     p->color.b = c[col].b;
     p->color.a = c[col].a;
+    p->round = 0;
     return 1;
 }
 
-void inherit_particle(particle *source, particle *target, SDL_Color *c)
+void inherit_particle(particle *source, particle *target)
 {
-    target->seed_x = (1 - (float)ADN_TRANSMISSION) * (target->seed_x) + (float)ADN_TRANSMISSION * source->seed_x;
-    target->seed_y = (1 - (float)ADN_TRANSMISSION) * (target->seed_y) + (float)ADN_TRANSMISSION * source->seed_y;
+    for(int i = 0; i < COMMAND_NUMBER ; i++)
+        if(rand()/(float)RAND_MAX < ADN_TRANSMISSION)//transmission
+            target->seed[i] = source->seed[i];
+        else//keep the command
+            target->seed[i] = target->seed[i];//=nothing
+    target->round = 0;
     target->x = WIDTH / 2;
     target->y = HEIGHT / 2;
     target->size = 20;
-    int col = rand() % NB_COLORS;
-    target->color.r = c[col].r;
-    target->color.g = c[col].g;
-    target->color.b = c[col].b;
-    target->color.a = c[col].a;
 }
 
 void copy_particle(particle *source, particle *target)
-{
-    target->seed_x = source->seed_x;
-    target->seed_y = source->seed_y;
+{   
+    for(int i = 0; i < COMMAND_NUMBER ; i++)
+        target->seed[i] = source->seed[i];
     target->x = source->x;
     target->y = source->y;
     target->size = source->size;
@@ -270,4 +278,19 @@ void copy_particle(particle *source, particle *target)
     target->color.g = source->color.g;
     target->color.b = source->color.b;
     target->color.a = source->color.a;
+    target->round = source->round;
+}
+
+
+void close_Particle(particle **p){
+    for(int i = 0; i < PARTICLES_NUMBER ; i++){
+        if((p)[i]->seed == NULL)
+            printf("line %d hasn't been destroyed yet\n", i);
+        else
+            free(p[i]->seed);
+    }
+    if(*p == NULL)
+        printf("particle array hasn't been destroyed yet\n");
+    else
+        free(*p);
 }
